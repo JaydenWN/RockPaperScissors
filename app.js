c50f364ae535081f3f19602fab68db5e16f18482
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2023-01-20 15:02:35 +1100

    Random AI Selection, User Input

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello World')
+let computerChoice;
+
+let getComputerChoice= (()=>{
+   let randNum = Math.floor(Math.random(3) * 3)
+   if(randNum === 0){
+    computerChoice = 'Rock'
+   }else  if(randNum === 1){
+    computerChoice = 'Paper'
+   }else{
+    computerChoice = 'Scissors'
+   }
+})();
+
+console.log(computerChoice)
+
+let userChoice = window.prompt('What is your choice, Rock | Paper | Scissors').toLowerCase()
+
+console.log(userChoice)
