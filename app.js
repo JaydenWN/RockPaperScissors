b88bdb4fdc500f6c33e2e4b9db029898bd1c786d
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2023-01-24 18:43:09 +1100

    Game interactivitiy with GUI

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+let compSelectSq = document.querySelector('#compSelectSq')
+let userSelectSq = document.querySelectorAll('.selectSq')
+let roundAnnouncement = document.querySelector('#roundAnnouncement')
+
+
 // Get how many rounds to play
-let chosenRounds = parseInt(window.prompt('How many rounds would you like to play?'))
+// let chosenRounds = parseInt(window.prompt('How many rounds would you like to play?'))
 
 // Get Computers Choice
 let computerChoice;
@@ -10,56 +15,64 @@ let userChoice;
 function getComputerChoice(){
    let randNum = Math.floor(Math.random(3) * 3)
    if(randNum === 0){
-    computerChoice = 'Rock'
     computerChoice = 'rock'
    }else  if(randNum === 1){
-    computerChoice = 'Paper'
     computerChoice = 'paper'
    }else{
-    computerChoice = 'Scissors'
     computerChoice = 'scissors'
    }
 };
-getComputerChoice()
 
 // Decide who wins
 
 function playGame(compChoice, useChoice){
     
     console.log(`Your Choice was ${userChoice} and the Computers Choice was ${computerChoice}`)
-    if(userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors' ){
-        return `${userChoice} is not a valid choice 😡 YOU LOOSE!`
-    }else if(computerChoice === userChoice){
-        return 'Its a draw'
-    }else if(computerChoice === 'rock' && userChoice ==='scissors'){
-        return 'You loose'
-    }else if(computerChoice === 'scissors' && userChoice ==='paper'){
-        return 'You loose'
-    }else if(computerChoice === 'paper' && userChoice ==='rock'){
-        return 'You loose'
+
+    if(useChoice !== 'rock' && useChoice !== 'paper' && useChoice !== 'scissors' ){
+        return console.log(`${userChoice} is not a valid choice 😡 YOU LOOSE!`)
+    }else if(compChoice === useChoice){
+        return roundAnnouncement.innerHTML = 'DRAW'
+    }else if(compChoice === 'rock' && useChoice ==='scissors'){
+        return roundAnnouncement.innerHTML = 'LOOSE'
+    }else if(compChoice === 'scissors' && useChoice ==='paper'){
+        return roundAnnouncement.innerHTML = 'LOOSE'
+    }else if(compChoice === 'paper' && useChoice ==='rock'){
+        return roundAnnouncement.innerHTML = 'LOOSE'
     }else{
-        return 'You Win'
+        return roundAnnouncement.innerHTML = 'WIN'
     }
 
 }
+
+//User Chooses, Computer Chooses, Computer shows display
+userSelectSq.forEach(ele =>{
+    ele.addEventListener('click', (e)=>{
+        userChoice = ele.innerHTML.toLowerCase()
+        getComputerChoice()
+        compSelectSq.innerHTML = computerChoice.toUpperCase()
+        playGame(computerChoice, userChoice)
+    })
+})
+
 
 // Lets user set how many rounds to play, if a string return invalid input.
 
-function game(numRounds){
-    if(chosenRounds > 0){
-        userChoice = window.prompt('What is your choice, Rock | Paper | Scissors').toLowerCase()
-        for(let i = 0; i < numRounds; i++){
-            console.log(playGame(computerChoice, userChoice))
-            getComputerChoice()
-            if(i < numRounds - 1){
-                userChoice = window.prompt('What is your choice, Rock | Paper | Scissors').toLowerCase()
-            }
+// function game(numRounds){
+//     if(chosenRounds > 0){
+//         userChoice = window.prompt('What is your choice, Rock | Paper | Scissors').toLowerCase()
+//         for(let i = 0; i < numRounds; i++){
+//             console.log(playGame(computerChoice, userChoice))
+//             getComputerChoice()
+//             if(i < numRounds - 1){
+//                 userChoice = window.prompt('What is your choice, Rock | Paper | Scissors').toLowerCase()
+//             }
             
-        }
-    }else{
-        console.log('Invalid input. Please type a number')
-    }
+//         }
+//     }else{
+//         console.log('Invalid input. Please type a number')
+//     }
     
-}
+// }
 
 game(chosenRounds)
